dd4ae21bb40e00788a1098cb75f31935fe830428
aldairmh98 <aldairmh@outlook.es>
2019-12-05 00:46:11 -0600

    Agregué sha a la lista de commits Versionado el: 05/12/2019 00:46:11

--- git_functions/commit_management.py
+++ git_functions/commit_management.py
@@ -7,10 +7,10 @@ repo = Repo('C:/Users/aldai/desarrollo/assistant_kirin/tutorial1')
 
 
 def commit_list(messageBody):
-    commits =  list(repo.iter_commits(max_count=1))
+    commits = list(repo.iter_commits(max_count=10))
     for c in commits:
         print(c)
-    return [{'name': str(c.author), 'email': c.author.email, 'msg': c.message} for c in commits]
+    return [{'sha': c.__str__(), 'name': str(c.author), 'email': c.author.email, 'msg': c.message} for c in commits]
 
 
 def version(messageBody):
